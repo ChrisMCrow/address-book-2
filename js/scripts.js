aa3b460cd524d17e359fa8683655a9dcef95e9cd
Chris Crow and Hyung Lee <chris@>
2018-08-21 09:03:01 -0700

    Add Address type, refactor JS, add hover effect instead of click.

--- js/scripts.js
+++ js/scripts.js
@@ -9,42 +9,52 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-function Address(street, city, state) {
+function Address(streetType, street, city, state) {
+  this.streetType = streetType;
   this.street = street;
   this.city = city;
   this.state = state;
 }
 
 Address.prototype.fullAddress = function() {
-  return this.street + ", " + this.city + ", " + this.state;
+  return this.streetType + ", " + this.street + ", " + this.city + ", " + this.state;
 }
 
 function resetFields() {
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street-type").val("");
     $("input.new-street").val("");
     $("input.new-city").val("");
     $("input.new-state").val("");
+}
+
+function appendAddressFields() {
+  $("#new-addresses").append('<div class="new-address">' +
+                              '<div class="form-group">' +
+                                '<label for="new-street-type">Address Type</label>' +
+                                '<input type="text" class="form-control new-street-type">' +
+                              '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-street">Street</label>' +
+                                 '<input type="text" class="form-control new-street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-city">City</label>' +
+                                 '<input type="text" class="form-control new-city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-state">State</label>' +
+                                 '<input type="text" class="form-control new-state">' +
+                               '</div>' +
+                             '</div>');
 }
 
 // User interface logic
 $(document).ready(function() {
 
   $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-street">Street</label>' +
-                                   '<input type="text" class="form-control new-street">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-city">City</label>' +
-                                   '<input type="text" class="form-control new-city">' +
-                                 '</div>' +
-                                 '<div class="form-group">' +
-                                   '<label for="new-state">State</label>' +
-                                   '<input type="text" class="form-control new-state">' +
-                                 '</div>' +
-                               '</div>');
+    appendAddressFields()
   });
 
   $("form#new-contact").submit(function(event) {
@@ -55,17 +65,20 @@ $(document).ready(function() {
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
     $(".new-address").each(function() {
+      var inputtedType = $(this).find("input.new-street-type").val();
       var inputtedStreet = $(this).find("input.new-street").val();
       var inputtedCity = $(this).find("input.new-city").val();
       var inputtedState = $(this).find("input.new-state").val();
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
-      newContact.addresses.push(newAddress)
+      var newAddress = new Address(inputtedType, inputtedStreet, inputtedCity, inputtedState)
+      if (!Object.values(newAddress).includes("")) {
+        newContact.addresses.push(newAddress);
+      }
     });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
+    $(".contact").last().hover(function() {
+      $("#show-contact").fadeToggle();
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
